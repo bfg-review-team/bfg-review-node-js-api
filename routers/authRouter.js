85ffabe8489bb2b1ef54cfbaf31e61f60ad8429d
aktan2778 <66529142+aktan2778@users.noreply.github.com>
2022-01-03 18:52:57 +0300

    Removed token from POST/user

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -40,7 +40,7 @@ router.get("/userAll", verifyToken, async (req, res) => {
     res.json(users);
 });
 
-router.post("/user", verifyToken, async (req, res) => {
+router.post("/user", async (req, res) => {
     const users = await userTrancactions.addUser(
     Object.assign(req.body)    
     );
