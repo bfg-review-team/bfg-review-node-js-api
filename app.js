d5cb97c8899d05ada7cbd88d9de92334b0dccb95
aktan2778 <66529142+aktan2778@users.noreply.github.com>
2022-01-03 17:34:26 +0300

    const to let

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-const PORT = process.env.PORT || 5000;
+let PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
