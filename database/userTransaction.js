be11fcf44042fa97eea431e7237bc6c92db8126d
aktan2778 <66529142+aktan2778@users.noreply.github.com>
2022-01-03 18:13:55 +0300

    final touches

--- database/userTransaction.js
+++ database/userTransaction.js
@@ -36,50 +36,6 @@ class UserTrancactions extends FadabHelper{
         console.log("Updating was successfully")
         return queryAsync("UPDATE Users SET UserName = ?, FilmName = ?, LastName = ?, Email = ?, Password = ?, AvatarUrl = ? WHERE Id = ?",body_data)
     }
-    /*getAllUserMovies(value) {
-        console.log('CallGetallusermovies!')
-        return queryAsync(`CALL GetUserMovies(?)`,value.PersonID)
-    }
-    getAllUserSeries(value) {
-        console.log('CallGetallusermovies!')
-        return queryAsync(`CALL GetUserSeries(?)`,value.PersonID)
-    }
-    addUserMovie(values) {
-        const body_data = [
-            values.PersonID,
-            values.tmdbid,
-            values.title,
-            values.director,
-            values.relasedate,
-            values.story,
-            values.acting,
-            values.editing,
-            values.music,
-            values.datewatch,
-            values.review
-        ]
-        console.log(body_data)
-        console.log('CallAddUserMovie!')
-        return queryAsync(`CALL AddUserMovie(?,?,?,?,?,?,?,?,?,?,?)`,body_data)   
-    }
-    addUserSeries(values) {
-        const body_data = [
-            values.PersonID,
-            values.tmdbid,
-            values.title,
-            values.producer, 
-            values.relasedate,
-            values.story,
-            values.acting,
-            values.editing,
-            values.music,
-            values.datewatch,
-            values.review
-        ]
-        console.log(body_data)
-        console.log('CallAddUserMovie!')
-        return queryAsync(`CALL AddUserSeries(?,?,?,?,?,?,?,?,?,?,?)`,body_data)   
-    }*/
 }
 
 module.exports = UserTrancactions;
