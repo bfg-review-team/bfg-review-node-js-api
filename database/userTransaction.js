860a23e27c92824706e9fe513396a819a8a49f43
Mert <mertbereket80@gmail.com>
2022-01-04 20:02:49 +0300

    refactor: faulty desing revised

--- database/userTransaction.js
+++ database/userTransaction.js
@@ -1,41 +1,58 @@
-const { FadabHelper , queryAsync, insertAsync } = require("fadab-mysql-helper");
+const { FadabHelper, queryAsync, insertAsync } = require("fadab-mysql-helper");
 
-class UserTrancactions extends FadabHelper{
-    constructor() {
-        super();
-        this.baseTable = "Users";
-    }
-    getAllUser() {
-        console.log('CallGetallusers!')
-        return queryAsync("SELECT * FROM Users")
-    }
-    getUser(values) {
-        const body_data = [values.Id]
-        console.log('Call Getuser!')
-        return queryAsync("SELECT * FROM Users WHERE Id = ? ",body_data)
-    }
-    addUser(values) {
-        const body_data = [
-            values.UserName,
-            values.Filmname,
-            values.LastName,
-            values.Email,
-            values.Password,
-            values.AvatarUrl
-        ]
-        console.log('CallAddNewUser!')
-        return queryAsync("INSERT INTO Users (UserName,FilmName,LastName,Email,Password,AvatarUrl) VALUES (?,?,?,?,?,?) ",body_data)   
-    }
-    deleteUser(values) {
-        const body_data = [values.UserName,values.Email]
-        console.log('Deleting This User !')
-        return queryAsync("DELETE FROM Users WHERE UserName = ? and Email = ? ",body_data)
-    }
-    updateUser(values) {
-        const body_data = [values.UserName,values.Filmname,values.LastName,values.Email,values.Password,values.AvatarUrl,values.Id]
-        console.log("Updating was successfully")
-        return queryAsync("UPDATE Users SET UserName = ?, FilmName = ?, LastName = ?, Email = ?, Password = ?, AvatarUrl = ? WHERE Id = ?",body_data)
-    }
+class UserTrancactions extends FadabHelper {
+  constructor() {
+    super();
+    this.baseTable = "Users";
+  }
+  getAllUser() {
+    console.log("CallGetallusers!");
+    return queryAsync("SELECT * FROM Users");
+  }
+  getUser(values) {
+    const body_data = [values.Id];
+    console.log("Call Getuser!");
+    return queryAsync("SELECT * FROM Users WHERE Id = ? ", body_data);
+  }
+  addUser(values) {
+    const body_data = [
+      values.UserName,
+      values.FirstName,
+      values.LastName,
+      values.Email,
+      values.Password,
+      values.AvatarUrl,
+    ];
+    console.log("CallAddNewUser!");
+    return queryAsync(
+      "INSERT INTO Users (UserName,FirstName,LastName,Email,Password,AvatarUrl) VALUES (?,?,?,?,?,?) ",
+      body_data
+    );
+  }
+  deleteUser(values) {
+    const body_data = [values.UserName, values.Email];
+    console.log("Deleting This User !");
+    return queryAsync(
+      "DELETE FROM Users WHERE UserName = ? and Email = ? ",
+      body_data
+    );
+  }
+  updateUser(values) {
+    const body_data = [
+      values.UserName,
+      values.FirstName,
+      values.LastName,
+      values.Email,
+      values.Password,
+      values.AvatarUrl,
+      values.Id,
+    ];
+    console.log("Updating was successfully");
+    return queryAsync(
+      "UPDATE Users SET UserName = ?, FirstName = ?, LastName = ?, Email = ?, Password = ?, AvatarUrl = ? WHERE Id = ?",
+      body_data
+    );
+  }
 }
 
 module.exports = UserTrancactions;
--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,89 +1,79 @@
 const UserTrancactions = require("../database/userTransaction");
-const ReviewTrancactions = require("../database/reviewTransaction")
-const MessageTrancactions = require("../database/messageTransaction")
-const DiscussionTrancactions = require("../database/discussionTransaction")
-const ListTrancactions = require("../database/listsTransaction")
-const listsTransactions = new ListTrancactions()
-const discussionTransactions = new DiscussionTrancactions()
+const ReviewTrancactions = require("../database/reviewTransaction");
+const MessageTrancactions = require("../database/messageTransaction");
+const DiscussionTrancactions = require("../database/discussionTransaction");
+const ListTrancactions = require("../database/listsTransaction");
+const listsTransactions = new ListTrancactions();
+const discussionTransactions = new DiscussionTrancactions();
 const userTrancactions = new UserTrancactions();
-const reviewTransactions = new ReviewTrancactions()
-const messageTransactions = new MessageTrancactions()
+const reviewTransactions = new ReviewTrancactions();
+const messageTransactions = new MessageTrancactions();
 const router = require("express")();
 const jwt = require("jsonwebtoken");
 const verifyToken = require("../middleware/verifyToken");
 
 router.post("/login", async (req, res) => {
-    const user = await userTrancactions.findOneAsync({
-    Email: req.body.Email,
-    Password: req.body.Pasword,
-}).catch();
-    if (!user) {
-    res.status(400).json("Kulşlanici adi ya da şifre ad kontrol ediniz");
+  const user = await userTrancactions
+    .findOneAsync({
+      Email: req.body.Email,
+      Password: req.body.Password,
+    })
+    .catch();
+  if (!user) {
+    res.status(400).json("Kullanıcı adı ya da şifre ad kontrol ediniz");
     return;
-    }
-    var token = jwt.sign({ PersonID: user.PersonID }, "secret", {
+  }
+  var token = jwt.sign({ PersonID: user.PersonID }, "secret", {
     expiresIn: "14d",
-    });
-    res.json({ user, token });
+  });
+  res.json({ user, token });
 });
 
 router.get("/user", verifyToken, async (req, res) => {
-    const users = await userTrancactions.getUser(
-    req.body
-    );
-    res.json(users);
+  const users = await userTrancactions.getUser(req.body);
+  res.json(users);
 });
 router.get("/userAll", verifyToken, async (req, res) => {
-    const users = await userTrancactions.getAllUser(
-    req.body
-    );
-    res.json(users);
+  const users = await userTrancactions.getAllUser(req.body);
+  res.json(users);
 });
 
 router.post("/user", async (req, res) => {
-    const users = await userTrancactions.addUser(
-    Object.assign(req.body)    
-    );
-    res.json(users);
+  const users = await userTrancactions.addUser(Object.assign(req.body));
+  res.json(users);
 });
 
 router.delete("/user", verifyToken, async (req, res) => {
-    const users = await userTrancactions.deleteUser(
-    Object.assign(req.body)    
-    );
-    res.json(users);
+  const users = await userTrancactions.deleteUser(Object.assign(req.body));
+  res.json(users);
 });
 
 router.put("/user", verifyToken, async (req, res) => {
-    const users = await userTrancactions.updateUser(
-    Object.assign(req.body)    
-    );
-    res.json(users);
+  const users = await userTrancactions.updateUser(Object.assign(req.body));
+  res.json(users);
 });
 
 router.get("/user/reviews", verifyToken, async (req, res) => {
-    const results = await reviewTransactions.getUserReviews(
-        Object.assign(req.body)
-    );
-    res.json(results);
+  const results = await reviewTransactions.getUserReviews(
+    Object.assign(req.body)
+  );
+  res.json(results);
 });
 router.get("/user/messages", verifyToken, async (req, res) => {
-    const results = await messageTransactions.getUserMessages(
-        Object.assign(req.body)
-    );
-    res.json(results);
+  const results = await messageTransactions.getUserMessages(
+    Object.assign(req.body)
+  );
+  res.json(results);
 });
 router.get("/user/discussions", verifyToken, async (req, res) => {
-    const results = await discussionTransactions.getUserDiscussions(
-        Object.assign(req.body)
-    );
-    res.json(results);
+  const results = await discussionTransactions.getUserDiscussions(
+    Object.assign(req.body)
+  );
+  res.json(results);
 });
 router.get("/user/lists", verifyToken, async (req, res) => {
-    const results = await listsTransactions.getUserLists(
-        Object.assign(req.body)
-    );
-    res.json(results);
+  const results = await listsTransactions.getUserLists(Object.assign(req.body));
+  res.json(results);
 });
 
 module.exports = router;
