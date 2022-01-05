9cedbcca5664a463b0e41cf73d1e6819dd56c1f4
Mert <mertbereket80@gmail.com>
2022-01-05 23:39:27 +0300

    refactor: message revised

--- database/messageTransaction.js
+++ database/messageTransaction.js
@@ -1,55 +1,62 @@
-const { FadabHelper , queryAsync, insertAsync } = require("fadab-mysql-helper");
+const { FadabHelper, queryAsync, insertAsync } = require("fadab-mysql-helper");
 
-class MessageTrancactions extends FadabHelper{
-    constructor() {
-        super();
-        this.baseTable = "Messages";
-    }
-    getAllMessages() {
-        console.log('CallGetallMessages!')
-        return queryAsync("SELECT * FROM Messages")
-    }
-    getMessage(values) {
-        const body_data = [values.Id]
-        console.log('Call GetMessage!')
-        return queryAsync("SELECT * FROM Messages WHERE Id=? ",body_data)
-    }
-    getUserMessages(values) {
-        const body_data = [values.UserId]
-        console.log('Call GetUserMessage!')
-        return queryAsync("SELECT * FROM Messages WHERE UserId=? ",body_data)
-    }
-    getDiscussionMessages(values) {
-        const body_data = [values.DiscussionId]
-        console.log('Call GetDiscussionMessage!')
-        return queryAsync("SELECT * FROM Messages WHERE DiscussionId=? ",body_data)
-    }
-    addMessage(values) {
-        const body_data = [
-            values.UserId,
-            values.MessageText,
-            values.DiscussionId,
-            values.CreatedDate
-        ]
-        console.log('CallAddNewMessage!')
-        return queryAsync("INSERT INTO Messages (UserId,MessageText,DiscussionId,CreatedDate) VALUES (?,?,?,?) ",body_data)   
-    }
-    deleteMessage(values) {
-        const body_data = [values.Id]
-        console.log('Deleting This Message !')
-        return queryAsync("DELETE FROM Messages WHERE Id = ? ",body_data)
-    }
-    updateMessage(values) {
-        const body_data = [
-            values.UserId,
-            values.MessageText,
-            values.DiscussionId,
-            values.CreatedDate,
-            values.Id
-        ]
-        console.log("Updating Message was successfully")
-        return queryAsync("UPDATE Messages SET UserId = ?, MessageText = ?, DiscussionId = ?,CreatedDate = ? WHERE Id = ?",body_data)
-    }
+class MessageTrancactions extends FadabHelper {
+  constructor() {
+    super();
+    this.baseTable = "Messages";
+  }
+  getAllMessages() {
+    console.log("CallGetallMessages!");
+    return queryAsync("SELECT * FROM Messages");
+  }
+  getMessage(Id) {
+    console.log("Call GetMessage!");
+    return queryAsync("SELECT * FROM Messages WHERE Id=? ", Id);
+  }
+  getUserMessages(values) {
+    const body_data = [values.UserId];
+    console.log("Call GetUserMessage!");
+    return queryAsync("SELECT * FROM Messages WHERE UserId=? ", body_data);
+  }
+  getDiscussionMessages(values) {
+    const body_data = [values.DiscussionId];
+    console.log("Call GetDiscussionMessage!");
+    return queryAsync(
+      "SELECT * FROM Messages WHERE DiscussionId=? ",
+      body_data
+    );
+  }
+  addMessage(values) {
+    const body_data = [
+      values.UserId,
+      values.MessageText,
+      values.DiscussionId,
+      values.CreatedDate,
+    ];
+    console.log("CallAddNewMessage!");
+    return queryAsync(
+      "INSERT INTO Messages (UserId,MessageText,DiscussionId,CreatedDate) VALUES (?,?,?,?) ",
+      body_data
+    );
+  }
+  deleteMessage(Id) {
+    console.log("Deleting This Message !");
+    return queryAsync("DELETE FROM Messages WHERE Id = ? ", [Id]);
+  }
+  updateMessage(values) {
+    const body_data = [
+      values.UserId,
+      values.MessageText,
+      values.DiscussionId,
+      values.CreatedDate,
+      values.Id,
+    ];
+    console.log("Updating Message was successfully");
+    return queryAsync(
+      "UPDATE Messages SET UserId = ?, MessageText = ?, DiscussionId = ?,CreatedDate = ? WHERE Id = ?",
+      body_data
+    );
+  }
 }
 
 module.exports = MessageTrancactions;
--- routers/messageRouter.js
+++ routers/messageRouter.js
@@ -1,47 +1,34 @@
-const MessageTrancactions = require("../database/messageTransaction")
-const MessageTransactions = new MessageTrancactions()
-const router  = require("express")();
+const MessageTrancactions = require("../database/messageTransaction");
+const MessageTransactions = new MessageTrancactions();
+const router = require("express")();
 const verifyToken = require("../middleware/verifyToken");
 
-router.get("/message", verifyToken, async (req, res) => {
-    const results = await MessageTransactions.getMessage(
-    req.body
-    );
-    res.json(results);
+router.get("/message/:Id", verifyToken, async (req, res) => {
+  const results = await MessageTransactions.getMessage(req.params.Id);
+  res.json(results);
 });
 router.get("/messagesAll", verifyToken, async (req, res) => {
-    const results = await MessageTransactions.getAllMessages(
-    req.body
-    );
-    res.json(results);
+  const results = await MessageTransactions.getAllMessages(req.body);
+  res.json(results);
 });
 router.get("/DiscussionMessages", verifyToken, async (req, res) => {
-    const results = await MessageTransactions.getDiscussionMessages(
-    req.body
-    );
-    res.json(results);
+  const results = await MessageTransactions.getDiscussionMessages(req.body);
+  res.json(results);
 });
 
 router.post("/message", verifyToken, async (req, res) => {
-    const results = await MessageTransactions.addMessage(
-    req.body
-    );
-    res.json(results);
+  const results = await MessageTransactions.addMessage(req.body);
+  res.json(results);
 });
 
 router.put("/message", verifyToken, async (req, res) => {
-    const results = await MessageTransactions.updateMessage(
-    req.body
-    );
-    res.json(results);
+  const results = await MessageTransactions.updateMessage(req.body);
+  res.json(results);
 });
 
-
-router.delete("/message", verifyToken, async (req, res) => {
-    const results = await MessageTransactions.deleteMessage(
-    req.body
-    );
-    res.json(results);
+router.delete("/message/:Id", verifyToken, async (req, res) => {
+  const results = await MessageTransactions.deleteMessage(req.params.Id);
+  res.json(results);
 });
 
-module.exports = router
+module.exports = router;
