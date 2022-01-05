05894c87de189b5edc5d6bc5787512062abd40fe
Mert <mertbereket80@gmail.com>
2022-01-05 23:39:49 +0300

    refactor: review revised

--- database/reviewTransaction.js
+++ database/reviewTransaction.js
@@ -1,77 +1,80 @@
-const { FadabHelper , queryAsync, insertAsync } = require("fadab-mysql-helper");
+const { FadabHelper, queryAsync, insertAsync } = require("fadab-mysql-helper");
 
-class ReviewTrancactions extends FadabHelper{
-    constructor() {
-        super();
-        this.baseTable = "Reviews";
-    }
-    getAllReviews() {
-        console.log('CallGetallReviews!')
-        return queryAsync("SELECT * FROM Reviews")
-    }
-    getReview(values) {
-        const body_data = [values.Id]
-        console.log('Call GetReview!')
-        return queryAsync("SELECT * FROM Reviews WHERE Id=? ",body_data)
-    }
-    getUserReviews(values) {
-        const body_data = [values.UserId]
-        console.log('Call GetUserReview!')
-        return queryAsync("SELECT * FROM Reviews WHERE UserId=? ",body_data)
-    }
-    getMovieReviews(values) {
-        const body_data = [values.MovieId]
-        console.log('Call GetUserReview!')
-        return queryAsync("SELECT * FROM Reviews WHERE MovieId=? ",body_data)
-    }
-    addReview(values) {
-        const body_data = [
-            values.UserId,
-            values.Title,
-            values.MovieId,
-            values.MessageText,
-            values.ReviewLike,
-            values.ReviewDislike,
-            values.CreatedDate
-        ]
-        console.log('CallAddNewReview!')
-        return queryAsync("INSERT INTO Reviews (UserId,Title,MovieId,MessageText,ReviewLike,ReviewDislike,CreatedDate) VALUES (?,?,?,?,?,?,?) ",body_data)   
-    }
-    deleteReview(values) {
-        const body_data = [values.Id]
-        console.log('Deleting This Review !')
-        return queryAsync("DELETE FROM Reviews WHERE Id = ? ",body_data)
-    }
-    updateReview(values) {
-        const body_data = [
-            values.UserId,
-            values.Title,
-            values.MovieId,
-            values.MessageText,
-            values.ReviewLike,
-            values.ReviewDislike,
-            values.CreatedDate,
-            values.Id
-        ]
-        console.log("Updating review was successfully")
-        return queryAsync("UPDATE Reviews SET UserId = ?, Title = ?, MovieId = ?, MessageText = ?, ReviewLike = ?, ReviewDislike = ?, CreatedDate = ? WHERE Id = ?",body_data)
-    }
-    updateReviewLike(values) {
-        const body_data = [
-            values.ReviewLike,
-            values.Id
-        ]
-        console.log("Updating review was successfully")
-        return queryAsync("UPDATE Reviews SET ReviewLike = ? WHERE Id = ?",body_data)
-    }
-    updateReviewDislike(values) {
-        const body_data = [
-            values.ReviewDislike,
-            values.Id
-        ]
-        console.log("Updating review was successfully")
-        return queryAsync("UPDATE Reviews SET ReviewDislike = ? WHERE Id = ?",body_data)
-    }
+class ReviewTrancactions extends FadabHelper {
+  constructor() {
+    super();
+    this.baseTable = "Reviews";
+  }
+  getAllReviews() {
+    console.log("CallGetallReviews!");
+    return queryAsync("SELECT * FROM Reviews");
+  }
+  getReview(Id) {
+    console.log("Call GetReview!");
+    return queryAsync("SELECT * FROM Reviews WHERE Id=? ", [Id]);
+  }
+  getUserReviews(UserId) {
+    console.log("Call GetUserReview!");
+    return queryAsync("SELECT * FROM Reviews WHERE UserId=? ", UserId);
+  }
+  getMovieReviews(values) {
+    const body_data = [values.MovieId];
+    console.log("Call GetUserReview!");
+    return queryAsync("SELECT * FROM Reviews WHERE MovieId=? ", body_data);
+  }
+  addReview(values) {
+    const body_data = [
+      values.UserId,
+      values.Title,
+      values.MovieId,
+      values.MessageText,
+      values.ReviewLike,
+      values.ReviewDislike,
+      values.CreatedDate,
+    ];
+    console.log("CallAddNewReview!");
+    return queryAsync(
+      "INSERT INTO Reviews (UserId,Title,MovieId,MessageText,ReviewLike,ReviewDislike,CreatedDate) VALUES (?,?,?,?,?,?,?) ",
+      body_data
+    );
+  }
+  deleteReview(Id) {
+    console.log("Deleting This Review !");
+    return queryAsync("DELETE FROM Reviews WHERE Id = ? ", [Id]);
+  }
+  updateReview(values) {
+    const body_data = [
+      values.UserId,
+      values.Title,
+      values.MovieId,
+      values.MessageText,
+      values.ReviewLike,
+      values.ReviewDislike,
+      values.CreatedDate,
+      values.Id,
+    ];
+    console.log("Updating review was successfully");
+    return queryAsync(
+      "UPDATE Reviews SET UserId = ?, Title = ?, MovieId = ?, MessageText = ?, ReviewLike = ?, ReviewDislike = ?, CreatedDate = ? WHERE Id = ?",
+      body_data
+    );
+  }
+  updateReviewLike(values) {
+    const body_data = [values.ReviewLike, values.Id];
+    console.log("Updating review was successfully");
+    return queryAsync(
+      "UPDATE Reviews SET ReviewLike = ? WHERE Id = ?",
+      body_data
+    );
+  }
+  updateReviewDislike(values) {
+    const body_data = [values.ReviewDislike, values.Id];
+    console.log("Updating review was successfully");
+    return queryAsync(
+      "UPDATE Reviews SET ReviewDislike = ? WHERE Id = ?",
+      body_data
+    );
+  }
 }
 
 module.exports = ReviewTrancactions;
--- routers/reviewRouter.js
+++ routers/reviewRouter.js
@@ -1,61 +1,45 @@
-const ReviewTrancactions = require("../database/reviewTransaction")
-const reviewTransactions = new ReviewTrancactions()
-const router  = require("express")();
+const ReviewTrancactions = require("../database/reviewTransaction");
+const reviewTransactions = new ReviewTrancactions();
+const router = require("express")();
 const verifyToken = require("../middleware/verifyToken");
 
-router.get("/review", verifyToken, async (req, res) => {
-    const results = await reviewTransactions.getReview(
-    req.body
-    );
-    res.json(results);
+router.get("/review/:Id", verifyToken, async (req, res) => {
+  const results = await reviewTransactions.getReview(req.params.Id);
+  res.json(results);
 });
 router.get("/reviewAll", verifyToken, async (req, res) => {
-    const results = await reviewTransactions.getAllReviews(
-    req.body
-    );
-    res.json(results);
+  const results = await reviewTransactions.getAllReviews(req.body);
+  res.json(results);
 });
 
 router.get("/movieReviewAll", verifyToken, async (req, res) => {
-    const results = await reviewTransactions.getMovieReviews(
-    req.body
-    );
-    res.json(results);
+  const results = await reviewTransactions.getMovieReviews(req.body);
+  res.json(results);
 });
 
 router.post("/review", verifyToken, async (req, res) => {
-    const results = await reviewTransactions.addReview(
-    req.body
-    );
-    res.json(results);
+  const results = await reviewTransactions.addReview(req.body);
+  res.json(results);
 });
 
 router.put("/review", verifyToken, async (req, res) => {
-    const results = await reviewTransactions.updateReview(
-    req.body
-    );
-    res.json(results);
+  const results = await reviewTransactions.updateReview(req.body);
+  res.json(results);
 });
 
 router.put("/review/like", verifyToken, async (req, res) => {
-    const results = await reviewTransactions.updateReviewLike(
-    req.body
-    );
-    res.json(results);
+  const results = await reviewTransactions.updateReviewLike(req.body);
+  res.json(results);
 });
 
 router.put("/review/dislike", verifyToken, async (req, res) => {
-    const results = await reviewTransactions.updateReviewDislike(
-    req.body
-    );
-    res.json(results);
+  const results = await reviewTransactions.updateReviewDislike(req.body);
+  res.json(results);
 });
 
-router.delete("/review", verifyToken, async (req, res) => {
-    const results = await reviewTransactions.deleteReview(
-    req.body
-    );
-    res.json(results);
+router.delete("/review/:Id", verifyToken, async (req, res) => {
+  const results = await reviewTransactions.deleteReview(req.params.Id);
+  res.json(results);
 });
 
-module.exports = router
+module.exports = router;
