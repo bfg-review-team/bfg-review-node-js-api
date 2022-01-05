abb20faa290caf2c2be40c8b99b16effab2ffb74
Mert <mertbereket80@gmail.com>
2022-01-05 23:38:53 +0300

    refactor: lists revised

--- routers/listsRouter.js
+++ routers/listsRouter.js
@@ -1,47 +1,35 @@
-const ListTrancactions = require("../database/listsTransaction")
-const listsTransactions = new ListTrancactions()
-const router  = require("express")();
+const ListTrancactions = require("../database/listsTransaction");
+const listsTransactions = new ListTrancactions();
+const router = require("express")();
 const verifyToken = require("../middleware/verifyToken");
 
-router.get("/list", verifyToken, async (req, res) => {
-    const results = await listsTransactions.getList(
-    req.body
-    );
-    res.json(results);
+router.get("/list/:Id", verifyToken, async (req, res) => {
+  const results = await listsTransactions.getList(req.params.Id);
+  res.json(results);
 });
-router.get("/listsAll", verifyToken, async (req, res) => {
-    const results = await listsTransactions.getAllLists(
-    req.body
-    );
-    res.json(results);
+router.get("/listsAll/:Id", verifyToken, async (req, res) => {
+  const results = await listsTransactions.getAllLists(req.params.Id);
+  res.json(results);
 });
 
 router.get("/movieListsAll", verifyToken, async (req, res) => {
-    const results = await listsTransactions.getMovieLists(
-    req.body
-    );
-    res.json(results);
+  const results = await listsTransactions.getMovieLists(req.body);
+  res.json(results);
 });
 
 router.post("/list", verifyToken, async (req, res) => {
-    const results = await listsTransactions.addList(
-    req.body
-    );
-    res.json(results);
+  const results = await listsTransactions.addList(req.body);
+  res.json(results);
 });
 
 router.put("/list", verifyToken, async (req, res) => {
-    const results = await listsTransactions.updateList(
-    req.body
-    );
-    res.json(results);
+  const results = await listsTransactions.updateList(req.body);
+  res.json(results);
 });
 
-router.delete("/list", verifyToken, async (req, res) => {
-    const results = await listsTransactions.deleteList(
-    req.body
-    );
-    res.json(results);
+router.delete("/list/:Id", verifyToken, async (req, res) => {
+  const results = await listsTransactions.deleteList(req.params.Id);
+  res.json(results);
 });
 
-module.exports = router
+module.exports = router;
