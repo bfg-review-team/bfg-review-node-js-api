cf90ed1499bd95aff37b8a8b44e84a361813e8dd
Mert <mertbereket80@gmail.com>
2022-01-05 23:38:42 +0300

    refactor: discussion revised

--- database/discussionTransaction.js
+++ database/discussionTransaction.js
@@ -1,55 +1,61 @@
-const { FadabHelper , queryAsync, insertAsync } = require("fadab-mysql-helper");
+const { FadabHelper, queryAsync, insertAsync } = require("fadab-mysql-helper");
 
-class DiscussionTrancactions extends FadabHelper{
-    constructor() {
-        super();
-        this.baseTable = "Discussions";
-    }
-    getAllDiscussions() {
-        console.log('CallGetallDiscussions!')
-        return queryAsync("SELECT * FROM Discussions").catch(e=>{console.log(e)})
-    }
-    getDiscussion(values) {
-        const body_data = [values.Id]
-        console.log('Call GetDiscussion!')
-        return queryAsync("SELECT * FROM Discussions WHERE Id=? ",body_data)
-    }
-    getUserDiscussions(values) {
-        const body_data = [values.UserId]
-        console.log('Call GetUserDiscussion!')
-        return queryAsync("SELECT * FROM Discussions WHERE UserId=? ",body_data)
-    }
-    getMovieDiscussions(values) {
-        const body_data = [values.MovieId]
-        console.log('Call GetMovieDiscussions!')
-        return queryAsync("SELECT * FROM Discussions WHERE MovieId=? ",body_data)
-    }
-    addDiscussion(values) {
-        const body_data = [
-            values.UserId,
-            values.Name,
-            values.MovieId,
-            values.CreatedDate
-        ]
-        console.log('CallAddNewDiscussion!')
-        return queryAsync("INSERT INTO Discussions (UserId,Name,MovieId,CreatedDate) VALUES (?,?,?,?) ",body_data)   
-    }
-    deleteDiscussion(values) {
-        const body_data = [values.Id]
-        console.log('Deleting This Discussion !')
-        return queryAsync("DELETE FROM Discussions WHERE Id = ? ",body_data)
-    }
-    updateDiscussion(values) {
-        const body_data = [
-            values.UserId,
-            values.Name,
-            values.MovieId,
-            values.CreatedDate,
-            values.Id
-        ]
-        console.log("Updating Discussion was successfully")
-        return queryAsync("UPDATE Discussions SET UserId = ?, Name = ?, MovieId = ?,CreatedDate = ? WHERE Id = ?",body_data)
-    }
+class DiscussionTrancactions extends FadabHelper {
+  constructor() {
+    super();
+    this.baseTable = "Discussions";
+  }
+  getAllDiscussions() {
+    console.log("CallGetallDiscussions!");
+    return queryAsync("SELECT * FROM Discussions").catch((e) => {
+      console.log(e);
+    });
+  }
+  getDiscussion(Id) {
+    console.log("Call GetDiscussion!");
+    return queryAsync("SELECT * FROM Discussions WHERE Id=? ", [Id]);
+  }
+  getUserDiscussions(values) {
+    const body_data = [values.UserId];
+    console.log("Call GetUserDiscussion!");
+    return queryAsync("SELECT * FROM Discussions WHERE UserId=? ", body_data);
+  }
+  getMovieDiscussions(values) {
+    const body_data = [values.MovieId];
+    console.log("Call GetMovieDiscussions!");
+    return queryAsync("SELECT * FROM Discussions WHERE MovieId=? ", body_data);
+  }
+  addDiscussion(values) {
+    const body_data = [
+      values.UserId,
+      values.Name,
+      values.MovieId,
+      values.CreatedDate,
+    ];
+    console.log("CallAddNewDiscussion!");
+    return queryAsync(
+      "INSERT INTO Discussions (UserId,Name,MovieId,CreatedDate) VALUES (?,?,?,?) ",
+      body_data
+    );
+  }
+  deleteDiscussion(Id) {
+    console.log("Deleting This Discussion !");
+    return queryAsync("DELETE FROM Discussions WHERE Id = ? ", [Id]);
+  }
+  updateDiscussion(values) {
+    const body_data = [
+      values.UserId,
+      values.Name,
+      values.MovieId,
+      values.CreatedDate,
+      values.Id,
+    ];
+    console.log("Updating Discussion was successfully");
+    return queryAsync(
+      "UPDATE Discussions SET UserId = ?, Name = ?, MovieId = ?,CreatedDate = ? WHERE Id = ?",
+      body_data
+    );
+  }
 }
 
 module.exports = DiscussionTrancactions;
--- routers/discussionRouter.js
+++ routers/discussionRouter.js
@@ -1,47 +1,39 @@
-const DiscussionTrancactions = require("../database/discussionTransaction")
-const discussionTransactions = new DiscussionTrancactions()
-const router  = require("express")();
+const DiscussionTrancactions = require("../database/discussionTransaction");
+const discussionTransactions = new DiscussionTrancactions();
+const router = require("express")();
 const verifyToken = require("../middleware/verifyToken");
 
-router.get("/discussion", verifyToken, async (req, res) => {
-    const results = await discussionTransactions.getDiscussion(
-    req.body
-    );
-    res.json(results);
+router.get("/discussion/:Id", verifyToken, async (req, res) => {
+  const results = await discussionTransactions.getDiscussion(req.params.Id);
+  res.json(results);
 });
 router.get("/discussionAll", verifyToken, async (req, res) => {
-    const results = await discussionTransactions.getAllDiscussions(
-    req.body
-    ).catch(e=>{console.log(e)});
-    res.json(results);
+  const results = await discussionTransactions
+    .getAllDiscussions(req.body)
+    .catch((e) => {
+      console.log(e);
+    });
+  res.json(results);
 });
 
 router.get("/moviediscussionAll", verifyToken, async (req, res) => {
-    const results = await discussionTransactions.getMovieDiscussions(
-    req.body
-    );
-    res.json(results);
+  const results = await discussionTransactions.getMovieDiscussions(req.body);
+  res.json(results);
 });
 
 router.post("/discussion", verifyToken, async (req, res) => {
-    const results = await discussionTransactions.addDiscussion(
-    req.body
-    );
-    res.json(results);
+  const results = await discussionTransactions.addDiscussion(req.body);
+  res.json(results);
 });
 
 router.put("/discussion", verifyToken, async (req, res) => {
-    const results = await discussionTransactions.updateDiscussion(
-    req.body
-    );
-    res.json(results);
+  const results = await discussionTransactions.updateDiscussion(req.body);
+  res.json(results);
 });
 
-router.delete("/discussion", verifyToken, async (req, res) => {
-    const results = await discussionTransactions.deleteDiscussion(
-    req.body
-    );
-    res.json(results);
+router.delete("/discussion/:Id", verifyToken, async (req, res) => {
+  const results = await discussionTransactions.deleteDiscussion(req.params.Id);
+  res.json(results);
 });
 
-module.exports = router
+module.exports = router;
