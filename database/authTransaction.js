fb7884bea9390e69f2dab819d636cfa03ed680b3
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2022-01-10 02:19:45 +0300

    refactor: AuthTrancactions revised.

--- database/authTransaction.js
+++ database/authTransaction.js
@@ -5,22 +5,6 @@ class AuthTrancactions extends FadabHelper {
     super();
     this.baseTable = "Users";
   }
-
-  addUser(values) {
-    const body_data = [
-      values.UserName,
-      values.FirstName,
-      values.LastName,
-      values.Email,
-      values.Password,
-      values.AvatarUrl,
-    ];
-    console.log("CallAddNewUser!");
-    return queryAsync(
-      "INSERT INTO Users (UserName,FirstName,LastName,Email,Password,AvatarUrl) VALUES (?,?,?,?,?,?) ",
-      body_data
-    );
-  }
 }
 
 module.exports = AuthTrancactions;
